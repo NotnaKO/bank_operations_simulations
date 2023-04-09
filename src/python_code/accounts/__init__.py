4014828fd8709402873c905931364501c5fa5680
NotnaKO <kopanovanton@gmail.com>
2023-04-09 19:49:07 +0300

    Add __init__.py to project modules

---
+++ src/python_code/accounts/__init__.py
@@ -0,0 +1,6 @@
+from .account_operations import *
+from .accounts import *
+from .accounts_factory import *
+from .banks import *
+from .codes_to_answers import *
+from .commissions import *
---
+++ src/python_code/data/__init__.py
@@ -0,0 +1,2 @@
+from .data_adapter import *
+from .serializable_base_class import *

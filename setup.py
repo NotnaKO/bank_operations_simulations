8536fb4fb986f80cb060c0486d67c76bd46bf09a
NotnaKO <kopanovanton@gmail.com>
2023-04-25 20:18:24 +0300

    Trying to add docker
    Trying to add docker into ci
    
    Trying to add docker into ci
    
    Trying to add docker into ci
    
    Trying to add docker into ci
    
    Trying to add docker into ci
    
    Trying to add docker into ci
    
    Trying to add docker into ci
    
    Trying to add docker into ci
    
    Trying to add docker into ci
    
    Trying to add docker into ci
    
    Trying to add docker into ci
    
    Trying to add docker into ci
    
    Trying to add docker into ci
    
    Trying to add docker into ci
    
    Trying to add docker into ci
    
    Trying to add docker into ci
    
    Trying to add docker into ci
    
    Trying to add docker

--- setup.py
+++ setup.py
@@ -1,18 +1,25 @@
 import os
 import sys
 
-from uml.make_uml import make_uml
 
-ver = sys.version_info
-try:
-    assert ver.major >= 3 and ver.minor >= 11
-except AssertionError:
-    print("Нужно установить python версии не ниже 3.11")
-    raise
-print("Starting installing requirements")
-os.system("python.exe -m pip install --upgrade pip")
-os.system("pip install -r requirements.txt")
+def check_requirements():
+    ver = sys.version_info
+    try:
+        assert ver.major == 3 and ver.minor >= 11
+    except AssertionError:
+        print("Нужно установить python версии не ниже 3.11")
+        raise
+    print("Starting installing requirements")
+    # os.system("python -m pip install --upgrade pip")
+    os.system("pip install -r requirements.txt")
 
-make_uml()
-# os.system("cls" if os.name == "nt" else "clear")
-print("Setup completed!")
+
+def make_uml_function():
+    from uml.make_uml import make_uml
+    make_uml()
+
+
+if __name__ == '__main__':
+    check_requirements()
+    make_uml_function()
+    print("Setup completed!")

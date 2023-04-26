4ba9a0196a82e6ee8399bf0c80b024e827d24a96
NotnaKO <kopanovanton@gmail.com>
2023-04-26 19:58:06 +0300

    data for tests fix
    Try to add badges
    
    data for tests fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,14 @@
 import json
+import pathlib
 
 import pytest
+
+data_path = pathlib.Path(__file__).parent / "data.json"
 
 
 @pytest.fixture
 def create_base(tmpdir):
-    with open("data.json") as file:
+    with open(data_path) as file:
         data = json.load(file)
     with open(tmpdir / "data.json", "w") as file:
         json.dump(data, file)
